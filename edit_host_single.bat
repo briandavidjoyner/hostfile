665a36da4889d3ed26b9893bdeb9117396d4270b
briandavidjoyner <brian@briandavidjoyner.com>
2016-07-26 16:00:52 -0400

    2

--- edit_host_single.bat
+++ edit_host_single.bat
@@ -1,4 +1,5 @@
 @echo off
-echo 52.2.24.193 bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 bounceexchange.com api.bounceexchange.com > %temp%\temphosts.txt
+
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
