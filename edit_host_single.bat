cddde41ee3037493247299b617b88bfb7ef364cc
briandavidjoyner <brian@briandavidjoyner.com>
2016-07-26 16:48:40 -0400

    Test

--- edit_host_single.bat
+++ edit_host_single.bat
@@ -1,8 +1,9 @@
 @echo off
+echo www.google.com golive.garden > %temp%\temphosts.txt
 echo 52.2.24.193 bounceexchange.com > %temp%\temphosts.txt
-echo api.bounceexchange.com > %temp%\temphosts.txt
-echo www.bounceexchange.com > %temp%\temphosts.txt
-echo core.bounceexchange.com > %temp%\temphosts.txt
-echo tag.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 api.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 www.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 core.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 tag.bounceexchange.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
