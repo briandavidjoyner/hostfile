2eac9169aa3e2ccdbbe85f0e85a5985d82e8d5ac
briandavidjoyner <brian@briandavidjoyner.com>
2016-07-26 16:44:41 -0400

    Sepearate

--- edit_host_single.bat
+++ edit_host_single.bat
@@ -1,4 +1,8 @@
 @echo off
-echo 52.2.24.193 bounceexchange.com api.bounceexchange.com www.bounceexchange.com core.bounceexchange.com tag.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 bounceexchange.com > %temp%\temphosts.txt
+echo api.bounceexchange.com > %temp%\temphosts.txt
+echo www.bounceexchange.com > %temp%\temphosts.txt
+echo core.bounceexchange.com > %temp%\temphosts.txt
+echo tag.bounceexchange.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
