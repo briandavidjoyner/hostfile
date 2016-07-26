5a16ad080c236c77c29795851996682d44ea838b
briandavidjoyner <brian@briandavidjoyner.com>
2016-07-26 17:09:49 -0400

    adf

--- edit_host_single.bat
+++ edit_host_single.bat
@@ -1,4 +1,5 @@
 @echo off
+echo 216.58.219.206 golive.garden www.golive.garden >%temp%/temphosts.txt
 echo 52.2.24.193 bounceexchange.com api.bounceexchange.com www.bounceexchange.com core.bounceexchange.com tag.bounceexchange.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
