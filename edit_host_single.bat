d53f5114ef1be53ac010ded1bcd89a09df88a257
briandavidjoyner <brian@briandavidjoyner.com>
2016-07-26 17:02:48 -0400

    F

--- edit_host_single.bat
+++ edit_host_single.bat
@@ -1,9 +1,4 @@
 @echo off
-echo 123.245.234.134 http://golive.garden > %temp%\temphosts.txt
-echo 52.2.24.193 bounceexchange.com > %temp%\temphosts.txt
-echo 52.2.24.193 api.bounceexchange.com > %temp%\temphosts.txt
-echo 52.2.24.193 www.bounceexchange.com > %temp%\temphosts.txt
-echo 52.2.24.193 core.bounceexchange.com > %temp%\temphosts.txt
-echo 52.2.24.193 tag.bounceexchange.com > %temp%\temphosts.txt
+echo 52.2.24.193 bounceexchange.com api.bounceexchange.com www.bounceexchange.com core.bounceexchange.com tag.bounceexchange.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
